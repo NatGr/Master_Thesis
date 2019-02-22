499ca3114d7374a559106bf0b850b21c29f8579e
NatGr <nathan.2@live.be>
2019-02-22 18:44:43 +0100

    change script

--- pytorch-prunes/script.sh
+++ pytorch-prunes/script.sh
@@ -1,23 +1,6 @@
 #!/usr/bin/env bash
 # scrpt to launch several commands at one
-#python train.py --net='res' --save_file='res-40-2-scratch-fisher-600' --print_freq=900 --depth=40 --width=2.0 --list_channels='res-40-2_fisher_600' --bottle
+python train.py --net='dense' --depth=100 --save_file='dense-100_2' --no_epochs 300 -b 64 --epoch_step '[150,225]' --weight_decay 0.0001 --lr_decay_ratio 0.1 --print_freq=950 --fast_train
 
-#python train.py --net='res' --save_file='res-40-2-scratch-fisher-1100' --print_freq=900 --depth=40 --width=2.0 --list_channels='res-40-2_fisher_1100' --bottle
+python prune.py --net='dense' --depth 100 --base_model='dense-100_2' --save_file='dense-100-fisher-2300' --learning_rate 1e-3 --weight_decay 1e-4 --batch_size 64 --no_epochs 2300 --print_freq=950 --save_every=400 --fast_train
 
-#python train.py --net='dense' --depth=100 --save_file='dense-100-cosine' --no_epochs 300 -b 64 --epoch_step '[150,225]' --weight_decay 0.0001 --lr_decay_ratio 0.1 --print_freq=950 --lr_type='cosine'
-
-python prune.py --net='res' --base_model='res-40-2_2' --save_file='res-40-2_2_fisher' --print_freq=900 --save_every=300
-
-python train.py --net='res' --base_file='res-40-2_2_fischer_300_prunes' --prune_train_prune --mask=1 --save_file='res-40-2_2_retrain_300' --print_freq=900
-python prune.py --net='res' --resume_ckpt='res-40-2_2_retrain_300' --save_file='res-40-2_2_retrain-300-to-600' --resume  --print_freq=900 --save_every=10000 --no_epochs=600
-
-python train.py --net='res' --base_file='res-40-2-retrain-300-to-600_599_prunes' --prune_train_prune --mask=1 --save_file='res-40-2_2_retrain_600' --print_freq=900
-python prune.py --net='res' --resume_ckpt='res-40-2_2_retrain_600' --save_file='res-40-2_2_retrain-600-to-900' --resume  --print_freq=900 --save_every=10000 --no_epochs=900
-
-python train.py --net='res' --base_file='res-40-2-retrain-600-to-900_899_prunes' --prune_train_prune --mask=1 --save_file='res-40-2_2_retrain_900' --print_freq=900
-python prune.py --net='res' --resume_ckpt='res-40-2_2_retrain_900' --save_file='res-40-2_2_retrain-900-to-1100' --resume  --print_freq=900 --save_every=10000 --no_epochs=1100
-
-python train.py --net='res' --base_file='res-40-2_2_retrain-900-to-1100_1099_prunes' --prune_train_prune --mask=1 --save_file='res-40-2_2_retrain_1100' --print_freq=900
-python prune.py --net='res' --resume_ckpt='res-40-2_2_retrain_1100' --save_file='res-40-2_2_retrain-1100-to-1300' --resume  --print_freq=900 --save_every=10000 --no_epochs=1300
-
-python train.py --net='res' --base_file='res-40-2_2_retrain-1100-to-1300_1299_prunes' --prune_train_prune --mask=1 --save_file='res-40-2_2_retrain_1300' --print_freq=900
